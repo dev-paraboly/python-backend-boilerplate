09a827382cf004a9e8fe76dad8c6a4da32e2c84c
sahn111 <you@example.com>
2022-11-07 11:25:39 +0300

    updates for boiler plate code

--- {{cookiecutter.app_name}}/{{cookiecutter.app_name.replace("-", "_")}}_service/api/v1/book_service/repository.py
+++ {{cookiecutter.app_name}}/{{cookiecutter.app_name.replace("-", "_")}}_service/api/v1/book_service/repository.py
@@ -1,4 +1,5 @@
 from sqlalchemy import select
+from fastapi import HTTPException
 
 from {{cookiecutter.app_name.replace("-", "_")}}_service.middlewares import db_session_middleware
 from {{cookiecutter.app_name.replace("-", "_")}}_service.models.book import Book
@@ -6,6 +7,9 @@ from {{cookiecutter.app_name.replace("-", "_")}}_service.models.book import Book
 
 class BookRepository:
     def get_book_query(self, book_id: str, session: db_session_middleware):
-        query = select(Book).where(Book.book_id == book_id)
-        result = session.execute(query).scalars().one()
-        return result
+        
+        query = session.query(Book).filter(Book.book_id == book_id).first()
+        if query :
+            return query
+        else:
+            raise HTTPException(status_code=404, detail=f"book item with id {book_id} not found")
--- {{cookiecutter.app_name}}/{{cookiecutter.app_name.replace("-", "_")}}_service/models/book.py
+++ {{cookiecutter.app_name}}/{{cookiecutter.app_name.replace("-", "_")}}_service/models/book.py
@@ -1,10 +1,20 @@
 import json
 
+from typing import Optional
+
+from enum import Enum
+
 from sqlalchemy import Column, String
 from sqlalchemy.orm import declarative_base
 
+from pydantic import BaseModel
+
 Base = declarative_base()
 from .helper import OrmHelper
+
+class FixedBookTypeExample(str, Enum):
+    HORROR = 'horror'
+    FANTASTIC = 'fantastic'
 
 class Book(Base):
     __tablename__ = 'book'
@@ -14,6 +24,7 @@ class Book(Base):
     author: str = Column(String, primary_key=True)
     book_name: str = Column(String, primary_key=False)
     description: str = Column(String, primary_key=False)
+    book_type: str = Column(String, primary_key=False)
 
     def __repr__(self) -> str:
         return json.dumps(self.dict())
@@ -23,3 +34,9 @@ class Book(Base):
 
     def dict(self) -> dict:
         return OrmHelper.toDict(self)
+
+class BookPydantic(BaseModel):
+    author : Optional[str]
+    book_name: Optional[str]
+    description: Optional[str]
+    book_type: Optional[FixedBookTypeExample]
