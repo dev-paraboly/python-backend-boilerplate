310ec5b244e4e0fba45d7a1fda651b1d45d46787
Abdulkadir <79089520+akilavuz@users.noreply.github.com>
2022-11-21 16:44:20 +0300

    Update create_openapi.py

--- {{cookiecutter.app_name}}/create_openapi.py
+++ {{cookiecutter.app_name}}/create_openapi.py
@@ -1,14 +1,14 @@
 from fastapi.openapi.utils import get_openapi
-from {{cookiecutter.app_name.replace("-", "_")}}_service.main import app
+from {{cookiecutter.app_name.replace("-", "_")}}_service.main import sub_app
 
 import json
 
 
 with open('openapi.json', 'w') as f:
     json.dump(get_openapi(
-        title=app.title,
-        version=app.version,
-        openapi_version=app.openapi_version,
-        description=app.description,
-        routes=app.routes,
+        title=sub_app.title,
+        version=sub_app.version,
+        openapi_version=sub_app.openapi_version,
+        description=sub_app.description,
+        routes=sub_app.routes,
     ), f)
